f47107ea25e928e93b0fa78b0d10c2fc3217df0c
grt-liaojinsha <721695214@qq.com>
2022-12-29 20:04:56 +0800

    设备列表开始开发

---
+++ app/src/main/java/com/olp/weco/ui/device/DeviceListActivity.kt
@@ -0,0 +1,13 @@
+package com.olp.weco.ui.device
+
+import android.os.Bundle
+import com.olp.weco.base.BaseActivity
+
+class DeviceListActivity :BaseActivity(){
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+}
